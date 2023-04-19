3d5c4a7df65decde7542c5976d34969ab781cc57
hugosdias <hugo_sdias@hotmail.com>
2023-04-18 21:10:55 -0300

    implementando a logica de xequemate

--- Tela.cs
+++ Tela.cs
@@ -14,10 +14,19 @@ namespace xadrex_console
             imprimirPecasCapturadas(partida);
 
             Console.WriteLine($"Turno: {partida.turno}");
-            Console.WriteLine($"Aguardando jogada: {partida.jogadorAtual}");
-            if(partida.xeque)
+            if(!partida.terminada)
             {
-                Console.WriteLine("XEQUE");
+
+                Console.WriteLine($"Aguardando jogada: {partida.jogadorAtual}");
+                if(partida.xeque)
+                {
+                    Console.WriteLine("XEQUE");
+                }
+            }
+            else
+            {
+                Console.WriteLine("XEQUE-MATE!");
+                Console.WriteLine($"Vencedor: {partida.jogadorAtual}");
             }
         }
 
